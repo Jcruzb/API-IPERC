ab4db1090da16cb518185f69fad09c4f13f77f1f
Juan Cruz <j.cruzb@outlook.com>
2024-02-17 19:05:21 +0100

    starting project

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const createError = require('http-errors');
+const { statusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+
+require('./config/db.config');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+    });
+
+app.use((error, req, res, next) => {
+    res.status(error.status || statusCodes.INTERNAL_SERVER_ERROR);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
